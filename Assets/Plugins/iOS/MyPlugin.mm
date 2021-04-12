42cfc70a4c19d09dd48b9983fa4f4f16bf844074
Vien Pham Quang <vienpq@nec.vn>
2021-04-12 16:46:53 +0700

    :+1: add iOS plugin test

---
+++ Assets/Plugins/iOS/MyPlugin.mm
@@ -0,0 +1,54 @@
+﻿#import <Foundation/Foundation.h>
+
+@interface MyPlugin : NSObject
+{
+    NSDate *createDate;
+}
+@end
+
+@implementation MyPlugin
+
+static MyPlugin* _shareInstance;
+
++(MyPlugin*) shareInstance
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"Create MyPlugin shareInstance");
+        _shareInstance = [[MyPlugin alloc] init];
+    });
+
+    return _shareInstance;
+}
+
+-(id)init
+{
+    self = [super init];
+    if (self)
+    {
+        [self initHelper];
+    }
+
+    return self;
+}
+
+-(void)initHelper
+{
+    NSLog(@"InitHelper called");
+    createDate = [NSDate date];
+}
+
+-(double)getElapsedTime
+{
+    return [[NSDate date] timeIntervalSinceDate:createDate];
+}
+
+@end
+
+extern "C"
+{
+    double IOS_getElapseTime()
+    {
+        return [[MyPlugin shareInstance] getElapsedTime];
+    }
+}
---
+++ Assets/Scripts/PluginTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PluginTest : MonoBehaviour
+{
+
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern double IOS_getElapsedTime();
+#endif
+
+    int frameCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Elapsed time: " + getElapsedTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+        if (frameCount >= 5) {
+            Debug.Log("Tick: " + getElapsedTime());
+            frameCount = 0;
+        }
+    }
+
+    double getElapsedTime() {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            return IOS_getElapsedTime();
+        }
+        Debug.LogWarning("Wrong platfrom");
+        return 0;
+    }
+}
